ec1b639379e743d4b9eeeef139d62d773aa4e14c
Yash Mishra <yashmishra@gmail.com>
2026-09-18 23:47:48 +0530

    chore: remove Tier 1 unused files

--- src/modules/m6_exam_engine/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . "/config/database.php";
-
-echo "<h1>InternBoot M6 Assessment Engine</h1>";
-echo "<p>PHP is working successfully.</p>";
-echo "<p style='color: green;'>MySQL database connected successfully.</p>";
-
-?>
